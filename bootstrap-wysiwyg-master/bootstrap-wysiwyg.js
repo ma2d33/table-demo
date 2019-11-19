0f4ba74f7b01a7cd57fa6579d312aa6e5119e374
madd <ma2d33@gmail.com>
2019-11-19 18:15:01 +0300

    extended wysiwig.js

--- bootstrap-wysiwyg-master/bootstrap-wysiwyg.js
+++ bootstrap-wysiwyg-master/bootstrap-wysiwyg.js
@@ -55,7 +55,7 @@
 						args = '<table class="table-bordered my-table">'+r+'</table>';
 						document.execCommand(command, 0, args);
 						updateToolbar();
-						die();
+						// die();
 					}
 					else{
 						document.execCommand(command, 0, args);
@@ -193,8 +193,77 @@
 				updateToolbar();
 			}
 		});
+
+		
+
 		return this;
 	};
+
+	$.fn.wysiwyg.table = function(){
+		// table extension
+		let editor = this;
+		editor.on('click',function(e){
+		if(e.target.nodeName != 'TD'){
+			cell_menu.hide(200);
+		}
+		else{
+		my_funct(e.target);
+		}
+		});
+
+		var tbl_cell;
+		var mouse_x,mouse_y;
+		var cell_menu = $("#tbl_options");
+
+		function my_funct(e){
+		  tbl_cell = e;		
+		  cell_menu.css({
+		  	'top':mouse_y,
+		  	'left':mouse_x
+		  });
+		  cell_menu.show(200);
+		}
+
+		$('#tbl_cell_left').click(function() {
+			$(tbl_cell).before('<td>New Cell</td>')
+			cell_menu.hide(200);
+		});
+		$('#tbl_cell_right').click(function() {
+			$(tbl_cell).after('<td>New Cell</td>')
+			cell_menu.hide(200);
+		});
+		$('#tbl_add_row').click(function() {
+			let num_cells = $(tbl_cell).parent('tr').children('td').length;
+			let cells = "";
+			for(num_cells;num_cells > 0; num_cells--){
+				cells +="<td>New Cell</td>"; 
+			}
+			$(tbl_cell).parent('tr').after('<tr>'+cells+'</tr>');
+			cell_menu.hide(200);
+		});
+		$('#tbl_cell_left_del').click(function() {
+			if($(tbl_cell).prev().length){
+				$(tbl_cell).prev().remove();
+				cell_menu.hide(200);
+			}
+		});
+		$('#tbl_cell_right_del').click(function() {
+			if($(tbl_cell).next().length){
+				$(tbl_cell).next().remove();
+				cell_menu.hide(200);
+			}
+		});
+		$('#tbl_row_del').click(function(){
+			$(tbl_cell).parent('tr').remove();
+			cell_menu.hide(200);
+		});
+
+		$(document).on('mousemove', e => {
+			mouse_x = e.pageX;
+			mouse_y = e.pageY;
+		}); // end table extension
+	};
+
 	$.fn.wysiwyg.defaults = {
 		hotKeys: {
 			'ctrl+b meta+b': 'bold',
